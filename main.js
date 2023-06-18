5803c0775980c39be9e04213a74c017de882ea9e
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-06-18 13:06:22 +0900

    카카오 로그인 팝업창 완성

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-var target = document.querySelectorAll(".btn_open");
-var btnPopClose = document.querySelectorAll(".pop_wrap .btn_close");
-var targetID;
-
-// 팝업 열기
-for (var i = 0; i < target.length; i++) {
-  target[i].addEventListener("click", function () {
-    targetID = this.getAttribute("href");
-    document.querySelector(targetID).style.display = "block";
-  });
-}
-
-// 팝업 닫기
-for (var j = 0; j < target.length; j++) {
-  btnPopClose[j].addEventListener("click", function () {
-    this.parentNode.parentNode.style.display = "none";
-  });
+function togglePopup() {
+  document.getElementById("popup-1").classList.toggle("active");
 }
